17e74ed82ca00d11cc08296fee5ecaaa15fb23f3
Jay Yang <jay.yijie.yang@gmail.com>
2022-06-10 19:18:47 +0800

    Macquarie path update

--- Macquarie/Path/settings.py
+++ Macquarie/Path/settings.py
@@ -2,6 +2,7 @@ path = __file__
 BASE_PATH = "C:/Users/JayYang"
 ONEDRIVE_PATH = f"{BASE_PATH}/OneDrive - Alpha Sherpa Capital"
 PLATFORM_PATH = f"{BASE_PATH}/Platform"
+CODE_PATH = PLATFORM_PATH
 DAILY_PATH = f"{ONEDRIVE_PATH}/Database/Daily"
 DATABASE_PATH = f"{ONEDRIVE_PATH}/Database/Macquarie"
 FINBERT_PATH = f"{ONEDRIVE_PATH}/Database"
